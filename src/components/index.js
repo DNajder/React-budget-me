7baa7893673e15b808f0e73ae2e9b7f9493958dd
Darek Najder <losman1987naj@gmail.com>
2020-05-15 00:36:55 +0200

    Add basic Navigation component

---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export {default as Navigation} from './navigation'
+export {default as Wrapper} from './Wrapper'
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Wrapper} from 'components';
+import {List, Container} from "./Navigation.css";
+import {Link} from "react-router-dom";
+
+
+const Navigation = ({items}) => {
+  return (
+    <Container>
+      <Wrapper>
+        <List>
+          {items.map(item=>(
+            <li key={item.to}>
+            <Link to={item.to}>{item.content}</Link>
+          </li>))}
+        </List>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navigation;
